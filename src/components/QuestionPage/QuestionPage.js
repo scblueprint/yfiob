28e1af56c397b2485217d5751f09536a263a4305
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2024-01-15 19:46:54 -0800

    Added QuestionPage stylesheet

--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -4,13 +4,6 @@ import styles from "./QuestionPage.module.css";
 const questionArray = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
-const answerArray = [
-  "Strongly Disagree",
-  "Disagree",
-  "Neutral",
-  "Agree",
-  "Strongly Agree",
-];
 
 export default function QuestionPage() {
   return (

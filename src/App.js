a5c9a7eae0c19963cda35289eed095493b1f7250
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2024-01-21 16:08:23 -0800

    slide 1 in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header'
+import Slide1 from './components/pages/slide1'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Slide1></Slide1>
     </div>
   );
 }
---
+++ src/components/pages/slide1.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './slide1.css';
+
+function Slide_1() {
+    return (
+        <div className="page">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"></link>
+              <div className="titleText">
+                    What Careers Can You Explore?
+              </div>
+              <div class = "filledContainer">
+                <div className="text">
+                    Let us help find how your interests can relate to different fields of work! Take this quiz to help give you ideas on what your career search can look like.
+                </div>
+                <div class = "innerBodyContainer">
+                  <div class = "innerBodyContainer" style = {{display: 'inline-flex' , flexDirection: 'column', justifyContent: 'space-evenly', gap: 20}}>
+                    <div class = "innerGreyBox">
+                      <div class = "text">
+                          Quick Assessment
+                      </div>
+                    </div>
+                    <div class = "text">
+                        This quick assessment consists of 6 questions, but will not be as representative of who you are.
+                    </div>
+                  </div>
+                  <div class = "innerBodyContainer" style = {{width: 283, height: 238, display: 'inline-flex' , flexDirection: 'column', justifyContent: 'space-evenly', gap: 20}}>
+                    <div class = "innerGreyBox">
+                        <div class = "text">
+                            Detailed Assessment
+                        </div>
+                    </div>
+                    <div class = "text">
+                      This detailed assessment consists of 60 questions.
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+      );
+}
+
+export default Slide_1;

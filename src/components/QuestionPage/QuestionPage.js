14ed846e0a1ac1621634acbea53aec2d48f4a13a
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2024-01-25 13:08:54 -0800

    Added some UI visuals to the question grid - No Functionality yet

--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -6,11 +6,11 @@ import { faArrowLeft, faChevronLeft, faChevronRight} from '@fortawesome/free-sol
 
 
 const questionArray = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  1, 2, 3, 4, 5, 6
 ];
 
 const answerArray = [
-  "strongly agree", "agree", "neutral", "disagree", "strongly disagree"
+  "strongly disagree", "disagree", "neutral", "agree", "strongly agree"
 ]
 
 

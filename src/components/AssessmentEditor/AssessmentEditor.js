d1c0e541f0486a0987e2094546be7bf54e8897ab
Jamie Quinn <djquinn@ucsc.edu>
2024-05-14 15:02:07 -0700

    Questions able to be fetched and displayed

--- src/components/AssessmentEditor/AssessmentEditor.js
+++ src/components/AssessmentEditor/AssessmentEditor.js
@@ -1,14 +1,38 @@
-import React, {} from "react";
-import "./AssessmentEditor.module.css";
+import React, { useState} from "react";
+import getQuestions from "../../firebase/pullQuestions";
+
+import styles from"./AssessmentEditor.module.css";
 
 
 function AssessmentEditor() {
-   
+    const [questions, setQuestions] = useState([]);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+          const questionsData = await getQuestions();
+          setQuestions(questionsData);
+        };
+        fetchData();
+      }, []);
+    
+    console.log(questions);
 
     return (
-       <div>
+        <div className={styles.questionBox}>
+            {questions.map((questionText, index) => {
+              const isSelected = false;
+              return (
+                <button
+                  className={`${styles.questionRow} ${isSelected ? styles.isSelected : ""}`}
+                  key={index}
+                  onClick={() => console.log("handleSelect(currentQuestionIndex, index)")}
+                >
+                  {questionText}
+                </button>
+              );
+            })}
+        </div>
 
-       </div>
     );
 }
 

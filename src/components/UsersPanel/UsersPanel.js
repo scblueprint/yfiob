8847f998f24a03bd7547d10f68347bf6e2178c47
Shaunveer Gill <shaunveergill@gmail.com>
2024-05-15 19:50:36 -0700

    table is now scrollable

--- src/components/UsersPanel/UsersPanel.js
+++ src/components/UsersPanel/UsersPanel.js
@@ -8,7 +8,6 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function UsersPanel() {
   const [users, setUsers] = useState([]);
-  const [selectedUserId, setSelectedUserId] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
@@ -23,22 +22,16 @@ function UsersPanel() {
     fetchUsers();
   }, []);
 
-  const handleUserClick = (userId) => {
-    // setSelectedUserId(userId);
-  };
-
-  const handleSearch = async () => { // Added async keyword to the function declaration
+  const handleSearch = async () => { 
     console.log("Search Query:", searchQuery);
     try {
-      const userData = await pullUsers({ name: searchQuery }); // Assuming pullUsers is correctly defined
+      const userData = await pullUsers({ name: searchQuery }); 
       setUsers(userData);
       console.log(userData);
     } catch (error) {
       console.error("Error fetching user data: ", error.message);
     }
   };
-
-  const selectedUser = users.find((user) => user.id === selectedUserId);
 
   return (
     <div className={styles.container}>
@@ -74,35 +67,24 @@ function UsersPanel() {
         </div>
       </div>
       <div className={styles.tableContainer}>
-        <table>
-          <tbody>
-          {users.map((user, index) => (
-              <tr key={user.id} onClick={() => handleUserClick(user.id)}>
-                <td className={styles.index}><div className={styles.cellContent}>{index + 1}</div></td>
-                <td className={styles.name}><div className={styles.cellContent}>{user.firstName}</div></td>
-                <td className={styles.grade}><div className={styles.cellContent}>{user.grade}</div></td>
-                <td className={styles.school}><div className={styles.cellContent}>{user.school}</div></td>
-                <td className={styles.zipcode}><div className={styles.cellContent}>{user.zipcode}</div></td>
-                <td className={styles.email}><div className={styles.cellContent}>{user.email}</div></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {/* {selectedUser && (
-        <div>
-          <h3>Selected User Details:</h3>
-          <p>
-            <strong>Name:</strong> {selectedUser.firstName}
-          </p>
-          <p>
-            <strong>Email:</strong> {selectedUser.email}
-          </p>
-          <p>
-            <strong>userId:</strong> {selectedUser.id}
-          </p>
+        <div className={styles.tableWrapper}>
+          <table>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user.id}>
+                  <td className={styles.index}>{index + 1}</td>
+                  <td className={styles.name}>{user.firstName}</td>
+                  <td className={styles.grade}>{user.grade}</td>
+                  <td className={styles.school}>{user.school}</td>
+                  <td className={styles.zipcode}>{user.zipcode}</td>
+                  <td className={styles.email}>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      )} */}
+      </div>
+
     </div>
   );
 }
